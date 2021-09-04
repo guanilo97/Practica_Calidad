1873382f0057a6b6c4a89a607aa3b348d463598e
jefri <you@example.com>
2021-09-03 21:38:27 -0500

    practica

---
+++ PracticaCalidad/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PracticaCalidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var utils = new StringUtilities();
+            int numero = Int32.Parse(Console.ReadLine());
+            int romano = utils.Romano(numero);
+            Console.WriteLine(romano);
+            //Console.WriteLine("centena"+ (numero / 100)%10);
+            //Console.WriteLine("desena"+ (numero / 10)%10);
+            //Console.WriteLine("unidad" + (numero / 1)%10);
+        }
+    }
+}
---
+++ PracticaCalidad/StringUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticaCalidad
+{
+    public class StringUtilities
+    {
+        public int Romano (int n)
+        {
+            int millar = ((n / 1000)%10);
+            int centena = ((n / 100) % 10);
+            int decena = ((n / 10) % 10);
+            int unidad = ((n / 1) % 10);
+            return millar;
+        }
+    }
+}
