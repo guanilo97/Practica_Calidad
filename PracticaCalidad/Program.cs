8047bae27c6fe1a932e1a7b805a8500a899016e0
jefri <you@example.com>
2021-09-03 21:59:53 -0500

    prueba

--- PracticaCalidad/Program.cs
+++ PracticaCalidad/Program.cs
@@ -8,8 +8,11 @@ namespace PracticaCalidad
         {
             var utils = new StringUtilities();
             int numero = Int32.Parse(Console.ReadLine());
-            int romano = utils.Romano(numero);
-            Console.WriteLine(romano);
+            int[] romano = utils.Romano(numero);
+            Console.WriteLine("millar" + romano[0]);
+            Console.WriteLine("centana" + romano[1]);
+            Console.WriteLine("desena" + romano[2]);
+            Console.WriteLine("unidad" + romano[3]);
             //Console.WriteLine("centena"+ (numero / 100)%10);
             //Console.WriteLine("desena"+ (numero / 10)%10);
             //Console.WriteLine("unidad" + (numero / 1)%10);
--- PracticaCalidad/StringUtilities.cs
+++ PracticaCalidad/StringUtilities.cs
@@ -6,13 +6,18 @@ namespace PracticaCalidad
 {
     public class StringUtilities
     {
-        public int Romano (int n)
+        public int[] Romano (int n)
         {
+            int[] arr = new int[4];
             int millar = ((n / 1000)%10);
-            int centena = ((n / 100) % 10);
-            int decena = ((n / 10) % 10);
-            int unidad = ((n / 1) % 10);
-            return millar;
+             int centena = ((n / 100) % 10);
+             int decena = ((n / 10) % 10);
+             int unidad = ((n / 1) % 10);
+            arr[0] = millar;
+            arr[1] = centena;
+            arr[2] = decena;
+            arr[3] = unidad;
+            return (arr);
         }
     }
 }
